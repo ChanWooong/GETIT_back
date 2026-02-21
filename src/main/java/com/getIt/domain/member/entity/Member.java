9c17838abff087a81d890294ef71977df8c61d22
chanwoong <oco466kr@knu.ac.kr>
2026-02-21 18:16:16 +0900

    feat : 테이블명 수정 (Users -> Members)

--- src/main/java/com/getIt/domain/member/entity/Member.java
+++ src/main/java/com/getIt/domain/member/entity/Member.java
@@ -4,6 +4,7 @@ import com.getit.domain.member.Role;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.math.BigInteger;
 import java.time.LocalDateTime;
 
 @Entity
@@ -11,7 +12,7 @@ import java.time.LocalDateTime;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Table(name = "Users")
+@Table(name = "Members")
 public class Member {
 
     @Id
@@ -21,18 +22,18 @@ public class Member {
     @Column(nullable = false, unique = true, length = 20)
     private Long studentId;
 
-    @Column(nullable = false)
-    private String password;
-
     @Column(nullable = false, length = 50)
     private String name;
 
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private Boolean isApproved = false; // 기본값 false , 관리자 승인 후  true로 변경
+
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
-    private Role role;
-
-    private String teamName;
-    private LocalDateTime createdAt;
+    private Role role; // GUEST, MEMBER, ADMIN만 존재
 
     @Builder
     public Member(Long studentId, String password, String name, Role role) {
@@ -40,5 +41,10 @@ public class Member {
         this.password = password;
         this.name = name;
         this.role = role;
+    }
+
+    public void approve(){
+        this.isApproved = true;
+        this.role = Role.ROLE_MEMBER;
     }
 }
